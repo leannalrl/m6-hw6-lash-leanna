b91f70378dae2c3a70f6399ffb31d48b35cd79da
Leanna <lrlash17@ufl.edu>
2020-10-11 23:08:24 -0400

    Changed the background colors of the boxes when each are hovered on

--- main.js
+++ main.js
@@ -21,3 +21,63 @@ document.getElementById("button3").addEventListener("click",
 		alert('Oops, butter luck next time');
 	}, false
 );
+
+//Change the background color of button 1 when the mouse enters the button
+document.getElementById("button1").addEventListener("mouseenter",
+	function(){
+		document.getElementById("button1").style.backgroundColor = "#cccccc"
+		document.getElementById("button1").style.color = "#000000";
+		document.getElementById("button2").style.backgroundColor = "#gold"
+		document.getElementById("button2").style.color = "#000000";
+		document.getElementById("button3").style.backgroundColor = "pink"
+		document.getElementById("button3").style.color = "#000000";
+	}, false
+);
+
+//Change the background color of button 1 back when the mouse leaves the button
+document.getElementById("button1").addEventListener("mouseleave",
+	function(){
+		document.getElementById("button1").style.backgroundColor = "red"
+		document.getElementById("button1").style.color = "#ffffff";
+	}, false
+);
+
+//Change the background color of button 2 when the mouse enters the button
+document.getElementById("button2").addEventListener("mouseenter",
+	function(){
+		document.getElementById("button1").style.backgroundColor = "gold"
+		document.getElementById("button1").style.color = "#000000";
+		document.getElementById("button2").style.backgroundColor = "#cccccc"
+		document.getElementById("button2").style.color = "#000000";
+		document.getElementById("button3").style.backgroundColor = "teal"
+		document.getElementById("button3").style.color = "#000000";
+	}, false
+);
+
+//Change the background color of button 2 back when the mouse leaves the button
+document.getElementById("button2").addEventListener("mouseleave",
+	function(){
+		document.getElementById("button2").style.backgroundColor = "yellow"
+		document.getElementById("button2").style.color = "#ffffff";
+	}, false
+);
+
+//Change the background color of button 3 when the mouse enters the button
+document.getElementById("button3").addEventListener("mouseenter",
+	function(){
+		document.getElementById("button1").style.backgroundColor = "teal"
+		document.getElementById("button1").style.color = "#000000";
+		document.getElementById("button2").style.backgroundColor = "#pink"
+		document.getElementById("button2").style.color = "#000000";
+		document.getElementById("button3").style.backgroundColor = "#cccccc"
+		document.getElementById("button3").style.color = "#000000";
+	}, false
+);
+
+//Change the background color of button 3 back when the mouse leaves the button
+document.getElementById("button3").addEventListener("mouseleave",
+	function(){
+		document.getElementById("button3").style.backgroundColor = "blue"
+		document.getElementById("button3").style.color = "#ffffff";
+	}, false
+);
