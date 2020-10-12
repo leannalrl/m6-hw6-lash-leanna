2b024dc8d397920650acaae15c26cc93aac5c84a
Leanna <lrlash17@ufl.edu>
2020-10-11 22:58:13 -0400

    Added the alerts for boxes when clicked on

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("button1").addEventListener("click",
+	function(){
+		alert('Oooh - so close, but no cigar');
+	}, false
+);
+
+document.getElementById("button2").addEventListener("click",
+	function(){
+		alert('DING DING DING - We have a winner');
+	}, false
+);
+
+document.getElementById("button2").addEventListener("click",
+	function(){
+		alert('DING DING DING - We have a winner');
+	}, false
+);
+
+document.getElementById("button3").addEventListener("click",
+	function(){
+		alert('Oops, butter luck next time');
+	}, false
+);
